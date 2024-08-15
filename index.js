3f11cab495b16ed50af88aec04489e15b33b696b
rifat1r <rifatul564@gmail.com>
2024-08-15 14:14:14 +0600

    added all booking api

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const port = process.env.PORT || 5000;
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const app = express();
+const port = process.env.PORT || 5000;
 
+// middleware
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
@@ -13,6 +15,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(cookieParser());
 
 const uri =
   "mongodb+srv://hotelBooking:guFER2WYYmLV19HU@cluster0.qfdpmw3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -25,6 +28,29 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+//middleware
+const varifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log("token in the middleware", token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log("token is not valid");
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
 async function run() {
   try {
@@ -39,17 +65,18 @@ async function run() {
     //auth related
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      console.log(user);
+      console.log("user for token", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logging out", user);
       res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: false,
-          sameSite: "none",
-        })
-        .send({ succeess: true });
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
     });
 
     app.get("/rooms", async (req, res) => {
@@ -71,8 +98,17 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/bookings", async (req, res) => {
-      // console.log(req.query?.email);
+    app.get("/allBookings", async (req, res) => {
+      const result = await bookingsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/bookings", varifyToken, async (req, res) => {
+      console.log("Cookies:", req.cookies);
+
+      if (req.user?.email !== req.query?.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
